c1773b81bee4191eabf0be027968da97ddf5de64
b13n1u <b13n1u@gmail.com>
2014-02-12 23:21:05 +0100

    Enable the service by default on Debian
    In order to run the sysstat at system start and enabling the collection of performance data you need to change the ```ENABLED``` parameter to ```true``` in ```/etc/default/sysstat```
    
    This can be simply done with augeas:  
    ```augeas { '/etc/default/sysstat':
        context => '/files/etc/default/sysstat/',
        changes => 'set ENABLED true',
        require => Package['sysstat'],
      }```

--- manifests/install.pp
+++ manifests/install.pp
@@ -11,7 +11,23 @@
 # Copyright (C) 2013 Joshua Hoblitt
 #
 class sysstat::install {
-  package { $sysstat::params::sysstat_package:
-    ensure => present,
-  }
+
+ case $::osfamily {
+  'debian': {
+              package { $sysstat::params::sysstat_package:
+                        ensure => present,
+                      }
+
+              augeas { '/etc/default/sysstat':
+              context => '/files/etc/default/sysstat/',
+              changes => 'set ENABLED true',
+              require => Package['sysstat'],}
+            }
+  default:  
+              package { $sysstat::params::sysstat_package:
+              ensure => present, }
+            }
+
+                }
 }
+
