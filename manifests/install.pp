f8463d87af8e16aa6215818bd141655161cb2a8a
b13n1u <b13n1u@gmail.com>
2014-02-13 20:22:22 +0100

    Update install.pp

--- manifests/install.pp
+++ manifests/install.pp
@@ -11,23 +11,21 @@
 # Copyright (C) 2013 Joshua Hoblitt
 #
 class sysstat::install {
+  case $::osfamily {
+    'debian': {
+      package { $sysstat::params::sysstat_package:
+      ensure => present, }
 
- case $::osfamily {
-  'debian': {
-              package { $sysstat::params::sysstat_package:
-                        ensure => present,
-                      }
-
-              augeas { '/etc/default/sysstat':
-              context => '/files/etc/default/sysstat/',
-              changes => 'set ENABLED true',
-              require => Package['sysstat'],}
-            }
-  default:  
-              package { $sysstat::params::sysstat_package:
-              ensure => present, }
-            }
-
-                }
+      augeas { '/etc/default/sysstat':
+      context => '/files/etc/default/sysstat/',
+      changes => 'set ENABLED true',
+      require => Package['sysstat'],}
+  }
+  default: {
+    package { $sysstat::params::sysstat_package:
+    ensure => present, }
+     }
+   }
 }
+
 
